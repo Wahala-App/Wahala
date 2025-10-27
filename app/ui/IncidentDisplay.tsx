60e6b2664ecf3f61000bd7874ba00a3c0df3e09f
Sentry2703 <adetunjiadewoye1@gmail.com>
2025-10-26 23:20:34 -0400

    Reverse Geolocation and Icon accompanying text

--- app/ui/IncidentDisplay.tsx
+++ app/ui/IncidentDisplay.tsx
@@ -1,25 +1,46 @@
 import { Incident } from "../api/types";
+import { IconText } from "@/app/ui/IconText";
 
 export default function IncidentDisplay(props: Incident)  {
-  return (
+
+    const onLocationCLick = () => {}
+
+    const onCorroborateClick = () => {}
+
+    const onDiscussClick = () => {}
+
+    return (
     <div className="mb-8">
       <div className="text-xl font-bold my-2">{props.title}</div>
 
       <div className="text-medium font-medium mb-2">{props.description}</div>
 
       <div className="flex justify-between">
-        <div className="font-thin">
-          <u>Location</u>
-        </div>{" "}
-        {/*TODO: Add link to location*/}
-        <div className="font-thin">
-          <u>Corroborate Crime</u>
-        </div>{" "}
-        {/*TODO: Add link to corroborate crime*/}
-        <div className="font-thin">
-          <u>Discuss</u>
-        </div>{" "}
-        {/*TODO: Add link to discuss*/}
+        <IconText
+            iconImage = "./iconText/location.svg"
+            onClick = {onLocationCLick}
+        >
+            <div className="font-thin">
+                <u>{"Location"}</u>
+            </div>
+        </IconText>
+
+        <IconText
+          iconImage = "./iconText/news.svg"
+          onClick = {onCorroborateClick}
+          >
+            <div className="font-thin">
+                <u>{"Corroborate Crime"}</u>
+            </div>
+        </IconText>
+
+        <IconText
+            iconImage={"./iconText/discuss.svg"}
+            onClick = {onDiscussClick}>
+            <div className="font-thin">
+                <u>{"Discuss Crime"}</u>
+            </div>
+        </IconText>
       </div>
     </div>
   );
